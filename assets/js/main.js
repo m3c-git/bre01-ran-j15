5dbe1040f5b14dac37f86b268b572e5aeca64ef0
m3c-git <ayyoubentreprise@gmail.com>
2023-11-03 11:32:14 +0000

    adding j15-ex3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 
+let bg = document.querySelectorAll("header > div")
+
+let bg2 = document.querySelectorAll("main > div > div")
+
+
 function selectColor(event)
 {
     let colorBox = event.target;
@@ -20,14 +25,10 @@ function getSelectedColor()
 function loadPalette(palette)
 {
     let colors = palette;
-    console.log(colors)
     // le code de l'étape 1 se passe ici
-    let bg = document.querySelectorAll("header > div")
-
     
     for (let i = 0; i < colors.length; i++)
     {
-            console.log(colors[i])
 
         bg[i].style.backgroundColor = `${colors[i]}`
     }
@@ -38,8 +39,27 @@ window.addEventListener("DOMContentLoaded", function(){
     loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
 
     // le code de l'étape 2 se passe ici
-
-
+    
+    
+     for (let i = 0; i < bg.length; i++)
+    {
+        bg[i].addEventListener("click", selectColor);
+    }
+    
     // le code de l'étape 3 se passe ici
+    
+    
+    
+    for (let j = 0; j < bg2.length; j++)
+    {
+        let color = getSelectedColor();
+        bg2[j].addEventListener("click", function(){
+            
+            console.log("toto")
+            bg2[j].style.backgroundColor = `${color}`;
+        });
+                    console.log(color)
+
+    }
 
 });
